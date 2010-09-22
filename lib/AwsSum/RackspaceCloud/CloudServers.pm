bc26eb0472ada430d4d46ddf653348a5c3357e2f
Andrew Chilton <andychilton@gmail.com>
2010-09-22 23:47:36 +1200

    Add 'get-server-details' and rename the previous two commands to something better

--- lib/AwsSum/RackspaceCloud/CloudServers.pm
+++ lib/AwsSum/RackspaceCloud/CloudServers.pm
@@ -17,6 +17,11 @@ has 'endpoint'   => ( is => 'rw', isa => 'Str' );
 # internal helpers
 has '_command' => ( is => 'rw', isa => 'HashRef' );
 
+# since URL changes dependent on the command, we'll just set it in each
+# command's method rather than creating it during execution. Then we just
+# return as usual it when asked.
+has '_url' => ( is => 'rw', isa => 'Str' );
+
 ## ----------------------------------------------------------------------------
 
 my $commands = {
@@ -33,17 +38,23 @@ my $commands = {
         method         => 'limits',
         http_method    => 'get',
     },
-    'servers' => {
-        name           => 'servers',
+    'list-servers' => {
+        name           => 'list-servers',
         path           => '/servers.json',
-        method         => 'servers',
+        method         => 'list_servers',
         verb           => 'get',
         code           => { 200 => 1, 203 => 1 },
     },
-    'servers-detail' => {
-        name           => 'servers-detail',
+    'list-servers-detail' => {
+        name           => 'list-servers-detail',
         path           => '/servers/detail.json',
-        method         => 'servers_detail',
+        method         => 'list_servers_detail',
+        verb           => 'get',
+        code           => { 200 => 1, 203 => 1 },
+    },
+    'get-server-details' => {
+        name           => 'get-server-details',
+        method         => 'get_server_details',
         verb           => 'get',
         code           => { 200 => 1, 203 => 1 },
     },
@@ -66,9 +77,19 @@ sub verb {
     my ($self) = @_;
     return $self->_command->{verb};
 }
+
 sub url {
     my ($self) = @_;
-    return $self->endpoint . $self->_command->{path};
+
+    # if we have made a URL then just return it
+    return $self->_url if $self->_url;
+
+    # else, use the predetermined one
+    if ( $self->_command->{path} ) {
+        return $self->endpoint . $self->_command->{path};
+    }
+
+    die "Program error: no URL created, therefore we don't know what to do";
 }
 
 sub code {
@@ -91,18 +112,37 @@ sub decode {
 ## ----------------------------------------------------------------------------
 # all our lovely commands
 
-sub servers {
+sub list_servers {
     my ($self, $params) = @_;
 
-    $self->set_command( 'servers' );
+    $self->set_command( 'list-servers' );
 
     return $self->send();
 }
 
-sub servers_detail {
+sub list_servers_detail {
     my ($self, $params) = @_;
 
-    $self->set_command( 'servers-detail' );
+    $self->set_command( 'list-servers-detail' );
+
+    return $self->send();
+}
+
+sub get_server_details {
+    my ($self, $params) = @_;
+
+    unless ( defined $params->{id} and $params->{id} =~ m{\d+}xms ) {
+        return {
+            _error => {
+                code => -1,
+                text => q{Please provide a valid integer for the server id ('id').},
+                desc => q{The id of the server should be provided in the input hash as 'id'.},
+            },
+        };
+    }
+
+    $self->set_command( 'get-server-details' );
+    $self->_url( $self->endpoint . '/servers/' . $params->{id} );
 
     return $self->send();
 }
