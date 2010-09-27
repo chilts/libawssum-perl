63c8b643634675da6cc97836741a048edc4f989a
Andrew Chilton <andychilton@gmail.com>
2010-09-28 12:21:40 +1300

    Useful method to flatten arrays into numbered parameters

--- lib/AwsSum/Amazon/Service.pm
+++ lib/AwsSum/Amazon/Service.pm
@@ -22,5 +22,24 @@ enum 'Endpoint' => qw(us-east-1 us-west-1 eu-west-1 ap-southeast-1);
 has 'endpoint' => ( is => 'rw', isa => 'Endpoint', default => 'us-east-1' );
 
 ## ----------------------------------------------------------------------------
+# helper functions specifically for Amazon services (not necessarily all of them)
+
+# useful for EC2 and SimpleDB (and maybe others)
+sub _amazon_add_flattened_array_to_params {
+    my ($self, $name, $array) = @_;
+
+    # count which key we're up to (start at 1)
+    my $i = 1;
+
+    # loop through everything, keep a number and save each key
+    foreach my $item ( @$array ) {
+        foreach my $key ( keys %$item ) {
+            $self->set_param( "$name.$i.$key", $item->{$key} );
+        }
+        $i++;
+    }
+}
+
+## ----------------------------------------------------------------------------
 1;
 ## ----------------------------------------------------------------------------
